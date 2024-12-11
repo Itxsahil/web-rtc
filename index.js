24d2a48997173d6b26f40497395510db5e8399c1
Sahil <132879371+Itxsahil@users.noreply.github.com>
2024-12-11 19:21:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",  
+    origin: "https://webrtc-client-rho.vercel.app",  
     methods: ["GET", "POST"],
   })
 );
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",  
+    origin: "https://webrtc-client-rho.vercel.app",  
     methods: ["GET", "POST"],
   },
 });
